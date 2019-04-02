17faec721af87d5f6d0c39aeb9f977ec48f60eec
phyoeminhein512 <phyoeminhein512@gmail.com>
2019-04-02 22:59:08 +0630

    Updates

---
+++ precache-manifest.77129849642458795d5257c0d479a3ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "17dfeddb3069a90388b1177cfe7b7e17",
+    "url": "/test/static/media/eiga_kanji.17dfeddb.svg"
+  },
+  {
+    "revision": "06ef5d3fdb8de5d4c9bf7df964e39409",
+    "url": "/test/static/media/eiga.06ef5d3f.svg"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "5d85fb0a28542ea21884",
+    "url": "/test/static/js/main.e2766756.chunk.js"
+  },
+  {
+    "revision": "bbb5ba78001ce504c6e9",
+    "url": "/test/static/js/2.adb65c57.chunk.js"
+  },
+  {
+    "revision": "5d85fb0a28542ea21884",
+    "url": "/test/static/css/main.d003358d.chunk.css"
+  },
+  {
+    "revision": "2b5e845cc4f8ef45645f90f6f1d705f7",
+    "url": "/test/index.html"
+  }
+];
